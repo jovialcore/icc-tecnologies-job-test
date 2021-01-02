f489634afcdc6026a900ee45d62eb6ffaeec8fc8
Chidiebere Chukwudi <jollydosky@gmail.com>
2021-01-02 14:50:34 +0100

    Finally uploaded the test

---
+++ demo/main.js
@@ -0,0 +1,32 @@
+const dataHtmlRes = document.getElementById('display');
+const button = document.getElementById('t');
+const loader = document.getElementById('fa');
+
+
+const url = 'http://universities.hipolabs.com/search?country=nigeria';
+
+
+button.addEventListener('click', function() {
+    //resets the response
+    button.innerText= " checking... ";
+    dataHtmlRes.innerText = "";
+
+
+fetch(url)
+    .then(function(response){
+        //removes the loader
+        
+         return response.text();
+    }).then(function(text){
+        button.innerText= " Done!";
+        dataHtmlRes.innerText = text;
+    })
+    .catch (function(error){
+        //removes the loader
+        button.innerText= " Done! Error";
+        dataHtmlRes.innerText = error.message;
+            // dataHtmlRes.innerText = error.message = "stuff worked men!";
+
+    })
+});
+
